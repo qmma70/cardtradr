7a15940b51bf908c62b77578b28208ab7d021b52
Qiming Ma <qmma@wireless1x-155-41-126-82.bu.edu>
2016-04-05 15:56:57 -0400

    Updated 100 MXN photo

--- CardTradr/app/src/main/java/com/example/qmma/featuredetection/MainActivity.java
+++ CardTradr/app/src/main/java/com/example/qmma/featuredetection/MainActivity.java
@@ -1,15 +1,9 @@
 package com.example.qmma.featuredetection;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.media.Image;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -21,25 +15,15 @@ import android.widget.Toast;
 import org.opencv.android.OpenCVLoader;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.concurrent.CountDownLatch;
 
 public class MainActivity extends AppCompatActivity {
     TextView text;
     Button button;
-    //ImageView imageView;
-    final int[] results = new int[ImageData.files.length];
 
     private String input2, picsDir;
 
     static final int REQUEST_TAKE_PHOTO = 2;
-
-    private static final int REQUEST_EXTERNAL_STORAGE = 1;
-    private static String[] PERMISSIONS_STORAGE = {
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,49 +31,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         text = (TextView) findViewById(R.id.text);
         button = (Button) findViewById(R.id.button);
-
-        File picsDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-        this.picsDir = picsDir.getAbsolutePath();
-        input2 = picsDir.getAbsolutePath() + File.separator + "input.jpg";
-        /*
-        int permission = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-
-        if (permission != PackageManager.PERMISSION_GRANTED) {
-            // We don't have permission so prompt the user
-            ActivityCompat.requestPermissions(
-                    this,
-                    PERMISSIONS_STORAGE,
-                    REQUEST_EXTERNAL_STORAGE
-            );
-        }
-
-        // installation. if this is the first time the app runs, copy files to picsDir.
-        // this step takes a lot of time.
-        for(int i = 0; i < ImageData.files.length; i++) {
-            String input1 = this.picsDir + File.separator + String.valueOf(i) + ".jpg";
-            if (! new File(input1).exists()) {
-                Bitmap bm_in1 = BitmapFactory.decodeResource(getResources(),
-                        ImageData.files[i]);
-                FileOutputStream out;
-                try {
-                    out = new FileOutputStream(input1);
-                    bm_in1.compress(Bitmap.CompressFormat.PNG, 100, out);
-                    if (out != null) {
-                        try {
-                            out.close();
-                        } catch (Exception e) {
-                            Toast.makeText(getApplicationContext(), "Could not read images in database.", Toast.LENGTH_SHORT).show();
-                            e.printStackTrace();
-                        }
-                    }
-                } catch (Exception e) {
-                    Toast.makeText(getApplicationContext(), "Could not read images in database.", Toast.LENGTH_SHORT).show();
-                    e.printStackTrace();
-                }
-            }
-        } // for
-        */
-
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -98,6 +39,10 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        File picsDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        this.picsDir = picsDir.getAbsolutePath();
+        input2 = picsDir.getAbsolutePath() + File.separator + "input.jpg";
 
         if (!OpenCVLoader.initDebug()) {
             Log.e(this.getClass().getSimpleName(), "  OpenCVLoader.initDebug(), not working.");
