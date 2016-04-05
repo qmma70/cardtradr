9383225854ad0eea7541dba3e50afe328036fe43
Qiming Ma <qmma@wireless1x-155-41-126-82.bu.edu>
2016-04-05 13:00:26 -0400

    Added comments

--- CardTradr/app/src/main/java/com/example/qmma/featuredetection/ImageData.java
+++ CardTradr/app/src/main/java/com/example/qmma/featuredetection/ImageData.java
@@ -4,6 +4,9 @@ package com.example.qmma.featuredetection;
  * Created by qmma on 16/4/5.
  */
 public class ImageData {
+
+    // Attention: add new images to the tails of arrays! Do not insert in the front or middle!
+
     // reference to files
     public static int[] files = new int[] {R.drawable.usd_100, R.drawable.usd_1, R.drawable.rmb_100, R.drawable.mxn_50};
     // name of the image
@@ -12,4 +15,5 @@ public class ImageData {
     public static String[] types = {"USD", "USD", "CNY", "MXN"};
     // value of the bill. If the image is not a bill, set it to 0.
     public static int[] values = new int[] {100, 1, 100, 50};
+    
 }
