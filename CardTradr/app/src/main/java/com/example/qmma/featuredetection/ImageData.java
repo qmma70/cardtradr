fc2eed3b74d2866ba2a15f64c2686d87315784c0
qmma <qmma70@gmail.com>
2016-04-09 15:49:50 -0400

    Now supports cards instead of money

--- CardTradr/app/src/main/java/com/example/qmma/featuredetection/ImageData.java
+++ CardTradr/app/src/main/java/com/example/qmma/featuredetection/ImageData.java
@@ -8,12 +8,11 @@ public class ImageData {
     // Attention: add new images to the tails of arrays! Do not insert in the front or middle!
     // reference to files
     public static int[] files = new int[]
-            {R.drawable.usd_100, R.drawable.usd_1, R.drawable.rmb_100, R.drawable.mxn_50, R.drawable.mxn_100, R.drawable.usd_20};
+            {R.drawable.mtg_aerial_predation, R.drawable.mtg_slum_reaper, R.drawable.mtg_golgari_keyrune,
+                    R.drawable.mtg_golgari_loglegs, R.drawable.mtg_sluiceway_scorpion};
     // name of the image
     public static String[] files_names =
-            {"100 USD bill", "1 USD bill", "100 RMB bill", "50 Mexican Pesos Bill", "100 Mexican Pesos Bill", "20 USD bill"};
-    // type of the image.
-    public static String[] types =
-            {"USD", "USD", "CNY", "MXN", "MXN", "USD"};
+            {"Magic the Gathering: Aerial Predation", "Magic the Gathering: Slum Reaper", "Magic the Gathering: Golgari Keyrune",
+                    "Magic the Gathering: Golgari Longlegs", "Magic the Gathering: Sluiceway Scorpion"};
 
 }
