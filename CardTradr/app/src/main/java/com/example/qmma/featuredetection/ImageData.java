40ecad452b2d1e45d79751c69153aabd277223e3
qmma <qmma70@gmail.com>
2016-04-09 16:08:06 -0400

    Added cards to the drawable folder and ImageData class

--- CardTradr/app/src/main/java/com/example/qmma/featuredetection/ImageData.java
+++ CardTradr/app/src/main/java/com/example/qmma/featuredetection/ImageData.java
@@ -9,10 +9,14 @@ public class ImageData {
     // reference to files
     public static int[] files = new int[]
             {R.drawable.mtg_aerial_predation, R.drawable.mtg_slum_reaper, R.drawable.mtg_golgari_keyrune,
-                    R.drawable.mtg_golgari_loglegs, R.drawable.mtg_sluiceway_scorpion};
+                    R.drawable.mtg_golgari_loglegs, R.drawable.mtg_sluiceway_scorpion, R.drawable.mtg_knight_of_glory,
+                    R.drawable.mtg_drudge_beetle, R.drawable.mtg_dead_reveler, R.drawable.mtg_stonefare_crocodille,
+                    R.drawable.mtg_destroy_evidence};
     // name of the image
     public static String[] files_names =
             {"Magic the Gathering: Aerial Predation", "Magic the Gathering: Slum Reaper", "Magic the Gathering: Golgari Keyrune",
-                    "Magic the Gathering: Golgari Longlegs", "Magic the Gathering: Sluiceway Scorpion"};
+                    "Magic the Gathering: Golgari Longlegs", "Magic the Gathering: Sluiceway Scorpion", "Magic the Gathering: Knight of Glory",
+                    "Magic the Gathering: Drudge Beetle", "Magic the Gathering: Dead Reveler", "Magic the Gathering: Stonefare Crocodile",
+                    "Magic the Gathering: Destroy the Evidence"};
 
 }
