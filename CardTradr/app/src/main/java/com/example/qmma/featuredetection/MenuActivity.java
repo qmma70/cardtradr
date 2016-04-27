1a5b5652efbae00237aa62b8fd4c2930cf26ca9c
Mark Benilov <benilovm@gmail.com>
2016-04-26 21:22:08 -0400

    Added the location feature. Also added some small UI changes ~ mostly small color changes.

--- CardTradr/app/src/main/java/com/example/qmma/featuredetection/MenuActivity.java
+++ CardTradr/app/src/main/java/com/example/qmma/featuredetection/MenuActivity.java
@@ -10,52 +10,29 @@ import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.Toast;
 import android.Manifest;
-import android .content.pm.PackageManager;
+import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.location.places.Place;
-import com.google.android.gms.location.places.PlaceLikelihood;
-import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
-import com.google.android.gms.location.places.Places;
-import com.google.android.gms.location.places.ui.PlacePicker;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Method;
 
-public class MenuActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+public class MenuActivity extends AppCompatActivity {
     private static final int PLACE_PICKER_REQUEST = 11;
     static final int REQUEST_TAKE_PHOTO = 2;
     private boolean doubleBackToExitPressedOnce = false;
-
-    //---------------------------------------------------------------------------
-    //Related to Google Places API
-    // source:http://www.truiton.com/2015/04/using-new-google-places-api-android/
-    private static final String LOG_TAG = "PlacesAPIActivity";
-    private static final int GOOGLE_API_CLIENT_ID = 0;
-    private GoogleApiClient mGoogleApiClient;
-    private static final int PERMISSION_REQUEST_CODE = 100;
-    //---------------------------------------------------------------------------
-
-    //Keep for testing purposes, but remove later.
-    //To do: possibly use current location here?
-    private static final LatLngBounds BOUNDS_MOUNTAIN_VIEW = new LatLngBounds(
-            new LatLng(37.398160, -122.180831), new LatLng(37.430610, -121.972090));
 
     //This function is called when btnTakePic is clicked.
     private void takePic() {
@@ -80,34 +57,13 @@ public class MenuActivity extends AppCompatActivity implements GoogleApiClient.O
         startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
     }
 
-    //This function is called when the mapImageButton is clicked.
-    //It launches the PlacePicker available through Google Places API.
-    //Source: http://www.truiton.com/2015/04/using-new-google-places-api-android/
-    private void openMap(){
-        try {
-            PlacePicker.IntentBuilder intentBuilder =
-                    new PlacePicker.IntentBuilder();
-            intentBuilder.setLatLngBounds(BOUNDS_MOUNTAIN_VIEW);
-            Intent intent = intentBuilder.build(MenuActivity.this);
-            startActivityForResult(intent, PLACE_PICKER_REQUEST);
-
-        } catch (GooglePlayServicesRepairableException
-                | GooglePlayServicesNotAvailableException e) {
-            e.printStackTrace();
-        }
-    }
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
 
-        mGoogleApiClient = new GoogleApiClient.Builder(MenuActivity.this)
-                .addApi(Places.PLACE_DETECTION_API)
-                .enableAutoManage(this, GOOGLE_API_CLIENT_ID, this)
-                .build();
+
 
         if (getIntent().hasExtra("AUTO")) {
             takePic();
@@ -138,21 +94,10 @@ public class MenuActivity extends AppCompatActivity implements GoogleApiClient.O
             }
 
         //If user selected mapImageButton...
-        } else if (requestCode == PLACE_PICKER_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                final Place place = PlacePicker.getPlace(this, data);
-                final CharSequence name = place.getName();
-                final CharSequence address = place.getAddress();
-            } else {
-                Log.e("CARD", "place picker failed");
-            }
         }
     }
 
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
-    }
 
     @Override
     public void onBackPressed() {
@@ -172,4 +117,26 @@ public class MenuActivity extends AppCompatActivity implements GoogleApiClient.O
             }
         }, 2000);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.location_menu_button, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.location_button){
+            Toast.makeText(MenuActivity.this, "Location button pressed", Toast.LENGTH_LONG).show();
+            Intent i = new Intent(getApplicationContext(), LocationActivity.class);
+            startActivity(i);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
